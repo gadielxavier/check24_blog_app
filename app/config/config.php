4d4b5ad22e593638c73f3051b35df13b28519fce
Gadiel Xavier Antunes Petronilo <gadielxavierantunespetronilo@MacBook-Pro-de-Gadiel.local>
2022-07-01 07:38:57 -0300

    First commit
    Project folders setup

---
+++ app/config/config.php
@@ -0,0 +1,16 @@
+<?php
+
+// DB Params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', 'root');
+define('DB_NAME', 'blog');
+
+// App Root
+define('APPROOT', dirname(dirname(__FILE__)));
+// URL Root
+define('URLROOT', 'http://localhost:8000/public');
+// Site Name
+define('SITENAME', 'Check24 Blog App');
+// App Version
+define('APPVERSION', '1.0.0');
