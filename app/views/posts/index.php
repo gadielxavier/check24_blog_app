49ecd7fa22fdea655e9747969e82491b6a23e4db
Gadiel Xavier Antunes Petronilo <gadielxavierantunespetronilo@MacBook-Pro-de-Gadiel.local>
2022-07-01 10:58:41 -0300

    Post index added

---
+++ app/views/posts/index.php
@@ -0,0 +1,8 @@
+<?php require APPROOT . '/views/inc/header.php';?>
+<div class="mt-4 p-5 bg-secondary text-center text-white rounded"">
+    <div class="container">
+        <h1 class="display-3"><strong>Check24 Blog App</strong>!</h1>
+        <p class="lead">You are logged in! Here the list of posts: </p>
+    </div>
+</div>
+<?php require APPROOT . '/views/inc/footer.php';?>
